a76d4301d8d8ffe79d2f7ae3b079cfad631a478d
Spenser Lee <Spenser.L159@gmail.com>
2016-03-21 06:02:54 -0700

    refactored marker creation

--- application/controllers/Map.php
+++ application/controllers/Map.php
@@ -2,23 +2,23 @@
 
 /**-----------------------------------------------------------------------
  * SOURCE FILE: controllers/Map.php
- *  
+ *
  * PROGRAM: GPSTracker
- * 
+ *
  * CLASS: Map
- * 
+ *
  * METHODS:
  * void index(username defaulted to null)
  * void loadMap(username)
- * 
+ *
  * DATE: March 10th, 2016
- * 
+ *
  * REVISIONS: March 20th, 2016: Commented
  *
  * @programmer				Carson Roscoe
- * 
+ *
  * @designer                            Carson Roscoe
- * 
+ *
  * NOTES: Controller used for drawing the map page
  * ----------------------------------------------------------------------*/
 
@@ -26,22 +26,22 @@ class Map extends Application {
 
     /**-------------------------------------------------------------------
      * METHOD: index
-     * 
+     *
      * INTERFACE: void index(username (optional))
-     * 
+     *
      * DATE: March 10th, 2016
-     * 
+     *
      * REVISIONS: March 20th, 2016: Commented
      *
      * @programmer: Carson Roscoe
-     * 
+     *
      * @designer: Carson Roscoe
-     * 
+     *
      * NOTES: When routing to the website.com/map page, this controller's
      * index method will take over. This method will handle generating the
      * page and displaying the map data for a given username(s)
      -------------------------------------------------------------------*/
-    public function index($name = null) { 
+    public function index($name = null) {
         $loggedName = $this->session->userdata('logged_in')['username'];
         if ($loggedName === NULL) {
             //handle nobody logged in and no profile username request
@@ -58,88 +58,104 @@ class Map extends Application {
             $fullName = $this->clients->getClientNamesByUsername($name);
             $this->data['contentTitle'] = 'Viewing '.$fullName[0].' '.$fullName[1].'\'s GPS Data';
         }
-        //$fullName = $this->players->getPlayerNamesByUsername($realName); //query players          
+        //$fullName = $this->players->getPlayerNamesByUsername($realName); //query players
 
         $this->loadMap($name);
-        
         $this->render();
     }
-    
+
     /**-------------------------------------------------------------------
      * METHOD: loadMap
-     * 
-     * INTERFACE: void loadmap(username)
-     * 
-     * DATE: March 10th, 2016
-     * 
-     * REVISIONS: March 20th, 2016: Commented
      *
-     * @programmer: Carson Roscoe
-     * 
+     * INTERFACE: void loadmap(username)
+     *
+     * DATE: March 10th, 2016
+     *
+     * REVISIONS: March 20th, 2016: Commented
+     *            March 20th, 2016: refactored - Spenser
+     *
+     * @programmer: Carson Roscoe, Spenser Lee
+     *
      * @designer: Carson Roscoe
-     * 
+     *
      * NOTES: Handles the Google Maps API calls and drawing a map
      * based on a given username.
      -------------------------------------------------------------------*/
     function loadMap($name) {
+
+        // center on BCIT
+        $config['center'] = 49.249458. ', ' . -123.001601;
+        $config['zoom'] = 12;
+        $this->googlemaps->initialize($config);
+
         if ($name != null) {
-            $coordinates = $this->coordinates->getLatestPositions($name);
-            $lastindex = count($coordinates)-1;
-            $latitude = $coordinates[$lastindex]->latitude;
-            $longitude = $coordinates[$lastindex]->longitude;
+            $latitude = $this->coordinates->getLatestPositions($name)[0]->latitude;
+            $longitude = $this->coordinates->getLatestPositions($name)[0]->longitude;
             $config['center'] = $latitude.', '.$longitude;
-            $config['zoom'] = 8;
             $this->googlemaps->initialize($config);
-            $marker = array();
-            $marker['position'] = $coordinates[$lastindex]->latitude.', '.$coordinates[$lastindex]->longitude;
-            $this->googlemaps->add_marker($marker);
-
-            $polyline = array();
-            $polyline['points'] = array();
-            $polyline['strokeColor'] = $this->clients->generateHexValueByName($name);
-            
-            foreach($coordinates as $coordinate) {
-                array_push($polyline['points'], $coordinate->latitude.', '.$coordinate->longitude);
-            }
-
-            $this->googlemaps->add_polyline($polyline);
-            $data['map'] = $this->googlemaps->create_map();
-
-            $this->data['content'] = $this->load->view('view_file', $data, true);
+            $this->loadMarkers($name);
         } else {
             $names = $this->clients->getClients();
-            $coordinates = $this->coordinates->getLatestPositions($this->session->userdata('logged_in')['username']);
-            $lastindex = count($coordinates)-1;
-            $latitude = $coordinates[$lastindex]->latitude;
-            $longitude = $coordinates[$lastindex]->longitude;
-            $config['center'] = $latitude.', '.$longitude;
-            $config['zoom'] = 7;
-            $this->googlemaps->initialize($config);
-            
-            foreach($names as $localname) {
-                $localcoordinates = $this->coordinates->getLatestPositions($localname[0]);
-                $lastindexlocal = count($localcoordinates)-1;
-                $locallatitude = $localcoordinates[$lastindexlocal]->latitude;
-                $locallongitude = $localcoordinates[$lastindexlocal]->longitude;
-                
-                $marker = array();
-                $marker['position'] = $locallatitude.', '.$locallongitude;
-                $this->googlemaps->add_marker($marker);
-                
-                $polyline = array();
-                $polyline['points'] = array();
-                $polyline['strokeColor'] = $this->clients->generateHexValueByName($localname[0]);
-
-                foreach($localcoordinates as $coordinate) {
-                    array_push($polyline['points'], $coordinate->latitude.', '.$coordinate->longitude);
-                }
-
-                $this->googlemaps->add_polyline($polyline);
+            foreach ($names as $localname) {
+                $this->loadMarkers($localname[0]);
             }
-
-            $data['map'] = $this->googlemaps->create_map();
-
-            $this->data['content'] = $this->load->view('view_file', $data, true);
         }
+
+        $data['map'] = $this->googlemaps->create_map();
+        $this->data['content'] = $this->load->view('view_file', $data, true);
+    }
+
+    /**-------------------------------------------------------------------
+     * METHOD: loadMarkers
+     *
+     * INTERFACE: void loadMarkers($name)
+     *
+     * DATE: March 20th, 2016
+     *
+     * REVISIONS: March 20th, 2016
+     *
+     * @programmer: Spenser Lee
+     *
+     * @designer: Spenser Lee
+     *
+     * NOTES: Helper function to draw map markers and polylines
+     -------------------------------------------------------------------*/
+    function loadMarkers($name) {
+        $coordinates = $this->coordinates->getAllPositions($name);
+        $firstLat = $coordinates[0]['latitude'];
+        $firstLon = $coordinates[0]['longitude'];
+
+        $marker = array();
+        $polyline = array();
+        $polyline['points'] = array();
+        $polyline['strokeColor'] = $this->clients->generateHexValueByName($name);
+
+        switch ($name) {
+            case 'slee':
+                $marker['icon'] = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+                break;
+            case 'mwillems':
+                $marker['icon'] = 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png';
+                break;
+            case 'tyu':
+                $marker['icon'] = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+                break;
+            case 'croscoe':
+                $marker['icon'] = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+                break;
+            default:
+                $marker['icon'] = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+                break;
+        }
+
+        foreach ($coordinates as $row) {
+            $latitude = $row['latitude'];
+            $longitude = $row['longitude'];
+
+            array_push($polyline['points'], $latitude . ', ' . $longitude);
+            $marker['position'] = $latitude . ', ' . $longitude;
+            $this->googlemaps->add_marker($marker);
+        }
+        $this->googlemaps->add_polyline($polyline);
     }
 }
--- data/gpstracker.sql
+++ data/gpstracker.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 03, 2016 at 06:51 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2016 at 02:00 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,72 +17,85 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `stockticker`
+-- Database: `gpstracker`
 --
-
-DROP TABLE IF EXISTS `clientlogin`;
-DROP TABLE IF EXISTS `clientposition`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `players`
+-- Table structure for table `clientlogin`
 --
 
-CREATE TABLE IF NOT EXISTS `clientlogin` (
-    `username` VARCHAR(30) NOT NULL,
-    `firstname` VARCHAR(30) NOT NULL,
-    `lastname` VARCHAR(30) NOT NULL,
-    `password` VARCHAR(30) NOT NULL,
-    PRIMARY KEY(`username`)
+CREATE TABLE `clientlogin` (
+  `username` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `players`
+-- Dumping data for table `clientlogin`
 --
 
 INSERT INTO `clientlogin` (`username`, `firstname`, `lastname`, `password`) VALUES
-('mwillems','Micah','Willems', 'password'),
-('croscoe','Carson', 'Roscoe', 'password'),
-('tyu','Thomas','Yu', 'password'),
-('aabdulla','Aman','Abdulla', 'password'),
-('slee','Spenser','Lee', 'password');
+('aabdulla', 'Aman', 'Abdulla', 'password'),
+('croscoe', 'Carson', 'Roscoe', 'password'),
+('mwillems', 'Micah', 'Willems', 'password'),
+('slee', 'Spenser', 'Lee', 'password'),
+('tyu', 'Thomas', 'Yu', 'password');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stocks`
+-- Table structure for table `clientposition`
 --
 
-CREATE TABLE IF NOT EXISTS `clientposition` (
-    `username` VARCHAR(30) NOT NULL,
-    `datetime` varchar(19) NOT NULL,
-    `latitude` DOUBLE(7,4) DEFAULT NULL,
-    `longitude` DOUBLE(7,4) DEFAULT NULL,
-    PRIMARY KEY(`username`, `datetime`)
+CREATE TABLE `clientposition` (
+  `username` varchar(30) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(15) NOT NULL,
+  `deviceName` varchar(64) NOT NULL,
+  `deviceId` varchar(64) NOT NULL,
+  `latitude` double(13,9) DEFAULT NULL,
+  `longitude` double(13,9) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `stocks`
+-- Dumping data for table `clientposition`
 --
 
-INSERT INTO `clientposition` (`username`, `datetime`, `latitude`, `longitude`) VALUES
-('croscoe', '2016.02.01-09:00:00', 49, -122),
-('croscoe', '2016.02.01-09:01:00', 49.3, -122.1),
-('croscoe', '2016.02.01-09:02:00', 49.2, -122.2),
-('croscoe', '2016.02.01-09:03:00', 49.3, -122.3),
-('croscoe', '2016.02.01-09:04:00', 49.5, -122.4),
-('croscoe', '2016.02.01-09:05:00', 49.6, -122.5),
-('croscoe', '2016.02.01-09:06:00', 49.4, -122.6),
-('croscoe', '2016.02.01-09:07:00', 49.2, -122.7),
-('croscoe', '2016.02.01-09:08:00', 49.3, -122.8),
-('croscoe', '2016.02.01-09:09:00', 49.1, -122.9),
-('croscoe', '2016.02.01-09:10:00', 49, -123),
-('aabdulla', '2016.02.01-09:03:00', 48.5, -122.5),
-('aabdulla', '2016.02.01-09:04:00', 48.6, -122.4),
-('aabdulla', '2016.02.01-09:05:00', 48.3, -122.6),
-('aabdulla', '2016.02.01-09:06:00', 48.8, -122.3),
-('aabdulla', '2016.02.01-09:07:00', 48.2, -122.6),
-('tyu', '2016.02.01-09:04:00', 48, -123),
-('mwillems', '2016.02.01-09:04:00', 48.5, -122),
-('slee', '2016.02.01-09:01:00', 49, -123);
+INSERT INTO `clientposition` (`username`, `datetime`, `ip`, `deviceName`, `deviceId`, `latitude`, `longitude`) VALUES
+('aabdulla', '2016-03-21 10:13:41', '96.49.6.113', 'Nexus 4', 'bf96379c9967f7d0', 49.162211000, -123.167920000),
+('aabdulla', '2016-03-21 10:13:49', '96.49.6.113', 'Nexus 4', 'bf96379c9967f7d0', 49.151144000, -123.185989000),
+('croscoe', '2016-03-21 10:13:59', '96.49.6.113', 'Nexus 4', 'bf96379c9967f7d0', 49.224628000, -123.150192000),
+('croscoe', '2016-03-21 10:16:59', '96.49.6.113', 'Nexus 4', 'bf96379c9967f7d0', 49.202018000, -122.981901000),
+('mwillems', '2016-03-21 05:42:02', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.249800000, -123.001600000),
+('mwillems', '2016-03-21 05:42:07', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.249800000, -123.001600000),
+('mwillems', '2016-03-21 05:42:28', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.249800000, -123.001600000),
+('mwillems', '2016-03-21 08:29:47', '142.232.144.240', 'X5pro', '9d692660b5aa8728', 49.249800000, -123.001700000),
+('slee', '2016-03-21 05:20:26', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.249800000, -123.001600000),
+('slee', '2016-03-21 11:31:26', '96.49.6.113', 'Nexus 6P', '5ae7f3222743908a', 49.150000000, -123.100000000),
+('slee', '2016-03-21 11:31:30', '96.49.6.113', 'Nexus 6P', '5ae7f3222743908a', 49.140526220, -123.192435310),
+('tyu', '2016-03-21 06:56:03', '142.232.139.214', 'LG-D852', 'b353dc16a0da6184', 49.246694000, -122.999342000),
+('tyu', '2016-03-21 08:17:04', '142.232.139.214', 'LG-D852', 'b353dc16a0da6184', 49.246430000, -123.008335000),
+('tyu', '2016-03-21 08:17:16', '142.232.139.214', 'LG-D852', 'b353dc16a0da6184', 49.250100000, -123.001300000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clientlogin`
+--
+ALTER TABLE `clientlogin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `clientposition`
+--
+ALTER TABLE `clientposition`
+  ADD PRIMARY KEY (`username`,`datetime`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
