60863d3b38c0eee81369ca96c7ff9085417d535e
Spenser Lee <Spenser.L159@gmail.com>
2016-03-20 22:54:58 -0700

    removed db config file

--- data/gpstracker.sql
+++ data/gpstracker.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14.1
+-- version 4.0.10.15
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 03, 2016 at 06:51 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Mar 21, 2016 at 05:47 AM
+-- Server version: 5.5.46
+-- PHP Version: 5.6.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,61 +14,70 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `stockticker`
+-- Database: `gpstracker`
 --
-
-DROP TABLE IF EXISTS `clientlogin`;
-DROP TABLE IF EXISTS `clientposition`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `players`
+-- Table structure for table `clientlogin`
 --
 
 CREATE TABLE IF NOT EXISTS `clientlogin` (
-    `username` VARCHAR(30) NOT NULL,
-    `firstname` VARCHAR(30) NOT NULL,
-    `lastname` VARCHAR(30) NOT NULL,
-    `password` VARCHAR(30) NOT NULL,
-    PRIMARY KEY(`username`)
+  `username` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `players`
+-- Dumping data for table `clientlogin`
 --
 
 INSERT INTO `clientlogin` (`username`, `firstname`, `lastname`, `password`) VALUES
-('mwillems','Micah','Willems', 'password'),
-('croscoe','Carson', 'Roscoe', 'password'),
-('tyu','Thomas','Yu', 'password'),
-('aabdulla','Aman','Abdulla', 'password'),
-('slee','Spenser','Lee', 'password');
+('aabdulla', 'Aman', 'Abdulla', 'password'),
+('croscoe', 'Carson', 'Roscoe', 'password'),
+('mwillems', 'Micah', 'Willems', 'password'),
+('slee', 'Spenser', 'Lee', 'password'),
+('tyu', 'Thomas', 'Yu', 'password');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stocks`
+-- Table structure for table `clientposition`
 --
 
 CREATE TABLE IF NOT EXISTS `clientposition` (
-    `username` VARCHAR(30) NOT NULL,
-    `datetime` varchar(19) NOT NULL,
-    `latitude` DOUBLE(7,4) DEFAULT NULL,
-    `longitude` DOUBLE(7,4) DEFAULT NULL,
-    PRIMARY KEY(`username`, `datetime`)
+  `username` varchar(30) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(15) NOT NULL,
+  `deviceName` varchar(64) NOT NULL,
+  `deviceId` varchar(64) NOT NULL,
+  `latitude` double(7,4) DEFAULT NULL,
+  `longitude` double(7,4) DEFAULT NULL,
+  PRIMARY KEY (`username`,`datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `stocks`
+-- Dumping data for table `clientposition`
 --
 
-INSERT INTO `clientposition` (`username`, `datetime`, `latitude`, `longitude`) VALUES
-('croscoe', '2016.02.01-09:03:00', 49, -122),
-('aabdulla', '2016.02.01-09:03:00', 48.5, -122.5),
-('tyu', '2016.02.01-09:04:00', 48, -123),
-('mwillems', '2016.02.01-09:04:00', 48.5, -122),
-('slee', '2016.02.01-09:01:00', 49, -123);
+INSERT INTO `clientposition` (`username`, `datetime`, `ip`, `deviceName`, `deviceId`, `latitude`, `longitude`) VALUES
+('aabdulla', '2016-02-01 09:03:00', '', '', '', 48.5000, -122.5000),
+('croscoe', '2016-02-01 09:03:00', '', '', '', 49.0000, -122.0000),
+('mwillems', '2016-02-01 09:04:00', '', '', '', 48.5000, -122.0000),
+('mwillems', '2016-03-21 05:42:02', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('mwillems', '2016-03-21 05:42:07', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('mwillems', '2016-03-21 05:42:28', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('slee', '2016-03-21 05:20:26', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('slee', '2016-03-21 05:38:21', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('slee', '2016-03-21 05:44:08', '142.232.163.46', 'Nexus 6P', '5ae7f3222743908a', 49.2498, -123.0016),
+('tyu', '2016-02-01 09:04:00', '', '', '', 48.0000, -123.0000);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
